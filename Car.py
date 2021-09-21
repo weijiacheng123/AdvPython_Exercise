dda2bad6ab9ed03773e2e072322d2781702f5aef
Weijia Cheng <weijia_cheng1@baylor.edu>
2021-09-20 21:22:41 -0500

    2nd

--- Car.py
+++ Car.py
@@ -1,23 +1,29 @@
 class car:
-    def __init__(self,make,model,year):
-        self.make = make
-        self.model = model
-        self.year = year
-        self.odometer_reading = 0
+    def __init__(self,make,model,speed):
+        self.__make = make
+        self.__year_model = model
+        self.__speed = 0
     
     def get_descriptive_name(self):
-        long_name = f"{self.year} {self.make} {self.model}"
+        long_name = f"{self.__year_model} {self.__make}, and your speed is {self.__speed}."
         return long_name.title()
 
-    def read_odometer(self):
-        print(f"This cas has {self.odometer_reading} mileos on it.")
+# accelerate - The accelerate method should add 5 to the speed data attribute each time it is called.
+    def car_accelerate(self,accelerate):
+        accelerate += self.__speed
 
-my_new_car = car('audi','a4',2019)
-print(my_new_car.get_descriptive_name())
-'''
-# change odometer directly
-my_new_car.odometer_reading = 23
-'''
-my_new_car.read_odometer()
+# brake - The brake method should subtract 5 from the speed data attribute each time it is called.
+    def car_brake(self,brake):
+        brake -= self.__speed
+
+#  get_speed - The get_speed method should return the current speed.
+    def get_speed(self,speed):
+        self.__speed = speed
+
+    
+        
+
+
+
 
 
--- CarProgram.py
+++ CarProgram.py
@@ -1,12 +1,28 @@
-import Car
+import Car as c
 
 def main():
-    car_maker = input("what's your car maker?\n")
-    car_model = input("what's your car model?\n")
-    car_year = input("when was your car made?\n")
+    car_make = input("what's the make of the car?\n")
+    car_model = input("what's the car's year model?\n")
+    car_speed = input("what's your speed now?\n")
 
-    my_new_car = Car.car(car_year,car_maker,car_model)
+    my_car = c.car(car_model,car_make,car_speed)
     #print(my_new_car.get_descriptive_name())
-    print(my_new_car.get_descriptive_name())
+    print(my_car.get_descriptive_name())
     
+    for accelerate in range(1,6):
+        my_car.car_accelerate(5)
+        my_car.get_speed(car_speed)
+        print(c.car_accelerate)
+        
+    for brake in range(1,6):
+        my_car.car_brake(5)
+        my_car.get_speed(car_speed)
+        print(c.car_brake)
+
+
 main()
+# creates a Car object then calls the accelerate method five times. After each call to the accelerate method, 
+# get the current speed of the car and display it. 
+
+# Then call the brake method five times. After each call to the brake method,
+# get the current speed of the car and display it.
